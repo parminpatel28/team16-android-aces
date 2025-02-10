6d741d33b486c1e9301b1359aeadc5bedf0fd110
Taylor Smith <taylor@eduroam-campus-10-36-250-10.campus-dynamic.uwaterloo.ca>
2025-02-10 14:34:17 -0500

    fix empty string ref

--- frontend/app/src/main/java/com/example/munchies/ui/friends/FriendsViewModel.kt
+++ frontend/app/src/main/java/com/example/munchies/ui/friends/FriendsViewModel.kt
@@ -7,7 +7,8 @@ import androidx.lifecycle.ViewModel
 class FriendsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "These are your friends!"
+        value = "These are your friends!" +
+                "You currently have none :("
     }
     val text: LiveData<String> = _text
 }
