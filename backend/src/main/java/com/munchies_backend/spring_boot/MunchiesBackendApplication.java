dd7d5b723f4113887f02291c0d235774fabde0af
ps26pate <ps26pate@uwaterloo.ca>
2025-02-11 14:27:46 -0500

    created a spring boot app for the backend

---
+++ backend/src/main/java/com/munchies_backend/spring_boot/MunchiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.munchies_backend.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchiesBackendApplication.class, args);
+	}
+
+}
