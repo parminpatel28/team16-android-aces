0792094ec0619b5d72aa8cff5d7e550a64431a92
Matthew Zhang <matthewzhang@Matthews-MacBook-Pro.local>
2025-03-04 02:15:59 -0500

    got map working

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.play.services.auth)
-
+    implementation(libs.play.services.maps)
+    implementation(libs.androidx.fragment.ktx)
     
 }
--- frontend/app/src/main/java/com/example/munchies/ui/map/MapFragment.kt
+++ frontend/app/src/main/java/com/example/munchies/ui/map/MapFragment.kt
@@ -7,22 +7,27 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.munchies.R
 import com.example.munchies.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
+        val mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -31,7 +36,20 @@ class MapFragment : Fragment() {
         mapViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // Initialize the map
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
         return root
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        val location = LatLng(43.4643, -80.5204) // Coordinates for Waterloo, Ontario
+        googleMap.addMarker(MarkerOptions().position(location).title("Marker in Waterloo"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
     }
 
     override fun onDestroyView() {
