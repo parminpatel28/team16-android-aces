95ac905f4d8e72433c8169a61c3fb249a4ecc072
Kailin Weng <Kailinnwenng@gmail.com>
2025-03-04 12:57:59 -0500

    frontend updates

--- frontend/app/src/main/java/com/example/munchies/ui/profile/ProfileFragment.kt
+++ frontend/app/src/main/java/com/example/munchies/ui/profile/ProfileFragment.kt
@@ -1,14 +1,18 @@
 package com.example.munchies.ui.profile
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.munchies.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
@@ -25,10 +29,26 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val logoutBuilder = AlertDialog.Builder(requireContext())
+        logoutBuilder.setTitle("Confirm logout?")
+        logoutBuilder.setMessage("")
+        logoutBuilder.setPositiveButton("Confirm") { dialog, which ->
+            profileViewModel.logout()
+        }
+        logoutBuilder.setNegativeButton("Cancel"){ dialog, which ->
+
+        }
+
+
+
         // Bind UI elements
         val textName: TextView = binding.textProfileName
         val textEmail: TextView = binding.textProfileEmail
+        val textReviews: TextView = binding.textProfileReviews
+        val textFriends: TextView = binding.textProfileFriends
+        val imagePfp: ImageView = binding.imageProfilePicture
         val btnLogout: Button = binding.btnLogout
+        val textBio: TextView = binding.textProfileBio
 
         // Observe data from ViewModel and update UI
         profileViewModel.userName.observe(viewLifecycleOwner) {
@@ -37,11 +57,25 @@ class ProfileFragment : Fragment() {
         profileViewModel.userEmail.observe(viewLifecycleOwner) {
             textEmail.text = it
         }
+        profileViewModel.userReviews.observe(viewLifecycleOwner) {
+            textReviews.text = "${it.size} reviews posted"
+        }
+        profileViewModel.userFriends.observe(viewLifecycleOwner) {
+            textFriends.text = "${it.size} friends"
+        }
+
+        profileViewModel.userPfp.observe(viewLifecycleOwner) {
+        }
+
+        profileViewModel.userBio.observe(viewLifecycleOwner){
+            textBio.text = it
+        }
 
         // Handle Logout Button Click
         btnLogout.setOnClickListener {
             // Call the ViewModel to handle logout logic
-            profileViewModel.logout()
+            //profileViewModel.logout()
+            logoutBuilder .create().show()
         }
 
         return root
--- frontend/app/src/main/java/com/example/munchies/ui/profile/ProfileViewModel.kt
+++ frontend/app/src/main/java/com/example/munchies/ui/profile/ProfileViewModel.kt
@@ -3,6 +3,7 @@ package com.example.munchies.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.munchies.model.Review
 
 class ProfileViewModel : ViewModel() {
 
@@ -13,11 +14,24 @@ class ProfileViewModel : ViewModel() {
     private val _userEmail = MutableLiveData<String>().apply { value = "john.doe@example.com" }
     val userEmail: LiveData<String> = _userEmail
 
+    private val _userReviews = MutableLiveData<List<Review>>().apply {value = listOf()}
+    val userReviews: LiveData<List<Review>> = _userReviews
+
+    private val _userFriends = MutableLiveData<List<Int>>().apply {value = listOf()}
+    val userFriends: LiveData<List<Int>> = _userFriends
+
+    private val _userBio = MutableLiveData<String>().apply {value = "I loooove eating food"}
+    val userBio: LiveData<String> = _userBio
+
+    private val _userPfp = MutableLiveData<String>().apply {value = "https://e.snmc.io/i/fullres/w/92a83a11be8d457d5fc32ac7477db0c3/11130567"}
+    val userPfp: LiveData<String> = _userPfp
+
     // Handle logout logic
     fun logout() {
         // Handle any necessary logout operations (clear session, tokens, etc.)
         // For now, we'll just print a message and clear the data for demonstration.
         _userName.value = "Logged out"
         _userEmail.value = "No email available"
+        _userBio.value = "..."
     }
 }
