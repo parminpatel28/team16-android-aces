82373214499564bd2a46f768858d23d364c78982
Matthew Zhang <matthewzhang@Matthews-MacBook-Pro.local>
2025-03-08 12:11:11 -0500

    added methods

--- backend/src/main/java/com/munchies_backend/spring_boot/controller/UserController.java
+++ backend/src/main/java/com/munchies_backend/spring_boot/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.munchies_backend.spring_boot.controller;
 
+import com.munchies_backend.spring_boot.model.User;
+import com.munchies_backend.spring_boot.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
 }
--- backend/src/main/java/com/munchies_backend/spring_boot/services/UserService.java
+++ backend/src/main/java/com/munchies_backend/spring_boot/services/UserService.java
@@ -1,14 +1,19 @@
 package com.munchies_backend.spring_boot.services;
 
+import com.munchies_backend.spring_boot.model.User;
 import com.munchies_backend.spring_boot.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    // Create user method
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
 }
