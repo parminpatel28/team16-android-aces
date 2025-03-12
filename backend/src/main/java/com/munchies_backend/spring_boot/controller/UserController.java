10fdc8d92cba4da3f05bc94f10ef7a0beecdbe0a
Matthew Zhang <matthewzhang@Matthews-MacBook-Pro.local>
2025-03-11 22:05:04 -0400

    backend changes

--- backend/src/main/java/com/munchies_backend/spring_boot/controller/UserController.java
+++ backend/src/main/java/com/munchies_backend/spring_boot/controller/UserController.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
