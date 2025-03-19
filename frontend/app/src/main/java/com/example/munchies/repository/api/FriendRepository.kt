f7f4b1644e9037e144b5538b0f5f0c5680da10b5
elainehan3 <elaineh7892@gmail.com>
2025-03-18 20:17:23 -0400

    pls

--- frontend/app/src/main/java/com/example/munchies/repository/api/FriendRepository.kt
+++ frontend/app/src/main/java/com/example/munchies/repository/api/FriendRepository.kt
@@ -17,8 +17,7 @@ class FriendRepository {
 
     private val apiService = ApiClient.userService
 
-    fun getUserFriends(userId: Int, onResult: (List<User>?) -> Unit) {
-        val gson = Gson()
+    fun getUserFriends(userId: String, onResult: (List<User>?) -> Unit) {
 
         Log.d("FriendRepository", "Getting friends for user $userId")
 
@@ -41,7 +40,7 @@ class FriendRepository {
 
     }
 
-    fun createFriend(userId: Int, friendId: Int, onResult: (Friendship?) -> Unit) {
+    fun createFriend(userId: String, friendId: Int, onResult: (Friendship?) -> Unit) {
         val gson = Gson()
 
         Log.d("FriendRepository", "Adding friend $friendId to user $userId")
